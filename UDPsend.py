771220aaa091ac331478904c153ea4ad4216c347
magicmmp <2233973404@qq.com>
2018-05-14 05:32:02 -0400

    UDPsend.py

---
+++ UDPsend.py
@@ -0,0 +1,11 @@
+import socket
+import time
+clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+data="data is from python."
+i=0
+while 1:
+    i=i+1
+    data="data is from python.times=%d"% i
+    clientSock.sendto(data, ('192.168.1.201', 58914))
+    time.sleep(1)
+clientSock.close()
