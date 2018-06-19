84e038d5dd9972b2f99b71b324cb06d75bf2e757
magicmmp <2233973404@qq.com>
2018-06-19 11:20:00 +0800

    add file: socket_use_to_connect_vxworks.txt

--- UDPsend.py
+++ UDPsend.py
@@ -1,11 +1,12 @@
 import socket
 import time
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-data="data is from python."
+data= [0 for i in range(2)]
 i=0
 while 1:
     i=i+1
-    data="data is from python.times=%d"% i
+    data[0]=:
+    buffer_to_send = struct.pack('>' + str(len(array_to_send_param)) + 'I', *array_to_send_param)
     clientSock.sendto(data, ('192.168.1.201', 58914))
     time.sleep(2)
 clientSock.close()
---
+++ udp_datasource_for_vxworks.c
@@ -0,0 +1,34 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <sys/socket.h>  
+#include <netinet/in.h>  
+#include <arpa/inet.h>  
+#include <netdb.h>   
+
+  
+int main(int argc, char** argv) {  
+    int socket_descriptor;
+    char buf[256];
+    int i;
+    int delay;
+    unsigned char n;
+    struct sockaddr_in address;
+    bzero(&address,sizeof(address));  
+    address.sin_family=AF_INET;  
+    address.sin_addr.s_addr=inet_addr("192.168.1.201");
+    address.sin_port=htons(58915);  
+    socket_descriptor=socket(AF_INET,SOCK_DGRAM,0);
+    n=0;
+    while(1)  
+    {
+      for(i=0;i<256;i++)
+        buf[i]=n;
+      n++;
+      sendto(socket_descriptor,buf,sizeof(buf),0,(struct sockaddr *)&address,sizeof(address));
+      for(delay=0;delay<65536*1024;delay++); 
+    }   
+    close(socket_descriptor);  
+    printf("Messages Sent\n");    
+}  
+
