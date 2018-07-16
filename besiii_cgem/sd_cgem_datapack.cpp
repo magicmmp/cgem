57c38f16706ff8f8d6b21a6eb3cc072dece75134
magicmmp <2233973404@qq.com>
2018-07-16 15:16:45 +0800

    cgem daq02 20190716

--- besiii_cgem/sd_cgem_datapack.cpp
+++ besiii_cgem/sd_cgem_datapack.cpp
@@ -19,40 +19,25 @@ int cgemDataPackCore(UINT *nDestination ,UINT *nSource,int *nBytes2BeSend)
 	register UINT *p_PointEveHead;
         int 	 nPerEventLen=0;
         int	 nRecordTrgNum;
-
 	        nRecordTrgNum=g_nTrgCounter;
-
                 p_PointEveHead=p_uDesti;
 		p_uDesti++;
-
 		g_fwRODHead.level1Id=nRecordTrgNum;
 		memcpy((char *)p_uDesti,&g_fwRODHead,sizeof(RODHeader));
 		p_uDesti=p_uDesti+(sizeof(RODHeader)>>2); /**p_uDesti is int pointer   */
-
 		nPerEventLen=nPerEventLen+sizeof(RODHeader)+4;
-
 		memcpy((char *)p_uDesti,(char*)p_uMove,*p_uMove) ;
         p_uDesti=p_uDesti+((*p_uMove)>>2);
-
         nPerEventLen=nPerEventLen+*p_uMove;
-
 		*p_uDesti=0;   /*status*/
 		p_uDesti++;
 		g_fwRODTail.numberOfDataElements=nPerEventLen;
-
 		memcpy((char *)p_uDesti,&g_fwRODTail,sizeof(RODTrailer));
 		p_uDesti=p_uDesti+(sizeof(RODTrailer)>>2);
 		nPerEventLen+=14;
-
 		*p_PointEveHead=nPerEventLen;  /*event head*/
-
-		
-
 		nRecordTrgNum++;
-
-
 	   g_nTrgCounter=nRecordTrgNum;
-
 	   *nBytes2BeSend=nPerEventLen;
 	return 0;
 }
